e87b36c09d53622e213bafd8f748ce9432e44eed
Davide Bertola <dade@dadeb.it>
2011-12-23 11:10:21 +0100

    Change licence to MIT

--- Plugins/Downloader.h
+++ Plugins/Downloader.h
@@ -1,10 +1,14 @@
-//
-//  Downloader.h
-//  Downloader
-//
-//  Created by Davide Bertola on 12/22/11.
-//  Copyright (c) 2011 Polito. All rights reserved.
-//
+/*
+ * Copyright (C) 2011 Davide Bertola
+ *
+ * Authors:
+ * Davide Bertola <dade@dadeb.it>
+ * Joe Noon <joenoon@gmail.com>
+ *
+ * This library is available under the terms of the MIT License (2008). 
+ * See http://opensource.org/licenses/alphabetical for full text.
+ */
+
 
 #import <Foundation/Foundation.h>
 
